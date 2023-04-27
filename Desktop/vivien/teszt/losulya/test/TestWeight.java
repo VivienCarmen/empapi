2b5d01fe075288c39489b767831df186144da5da
Kisgyorgy Bendeguz <kisgyorgybeni@gmail.com>
2023-04-27 17:53:58 +0200

    sikerulj

---
+++ Desktop/vivien/teszt/losulya/test/TestWeight.java
@@ -0,0 +1,15 @@
+package losulya.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestWeight {
+    @Test
+    
+    public void testValami(){
+        assertEquals(1, 1);
+
+    }
+}
